8e87b2b2acd71473703cb862486227c5edc04a53
zwp <1258271806@qq.com>
2018-03-09 16:16:01 +0800

    初始化工程

---
+++ README.md
@@ -0,0 +1,2 @@
+# HuaweiMatch
+华为精英挑战赛测试代码，python
---
+++ src/test/test1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年3月9日
+
+@author: zwp
+'''
+
+if __name__ == '__main__':
+    print 'hello world'
+    pass;
+
