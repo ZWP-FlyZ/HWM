cc89721d9b5fe4d2ae0cdf28f9d8db26fa4ffc3c
zwp <1258271806@qq.com>
2018-03-11 22:57:07 +0800

    补充完集群类操作

--- src/main/packing_model.py
+++ src/main/packing_model.py
@@ -51,12 +51,53 @@ class MachineGroup():
         '''
         初始化集群，创建一个物理机，并初始化相关参数
         '''
-        
+        self.machine_info['CPU']= caseInfo.CPU;
+        self.machine_info['MEM']= caseInfo.MEM;
+        self.machine_info['HDD']= caseInfo.HDD;
+        self.new_physic_machine();
         pass;
 
+    def new_physic_machine(self):
+        '''
+        创建一个新的物理机，
+        '''
+        self.pm_size+=1;
+        npm = {
+            're_cpu':self.machine_info['CPU'],
+            're_mem':self.machine_info['MEM'],
+            'vm_size':0
+            };
+        self.PM_status.append(npm);
+        self.PM.append({});
 
-
-
+    def put_vm(self,pm_id,vm_type):
+        '''
+        放置一个虚拟机，
+        pm_id为物理机ID，vm_type为虚拟机类型名
+        如果放置成功 返回放置后的物理机(re_cpu,re_mem)，
+        因空间不足放置失败返回None
+        '''
+        if pm_id is None or \
+        pm_id<0 or pm_id>=self.pm_size:
+            raise ValueError('error pm_id=',pm_id);
+        vm_cpu,vm_mem = ParamInfo.VM_PARAM[vm_type][:2];
+        pmstatus = self.PM_status[pm_id];
+        re_cpu = pmstatus['re_cpu']-vm_cpu;
+        re_mem = pmstatus['re_mem']-vm_mem;
+        if re_cpu>=0 and  re_mem>=0:
+            pmstatus['re_cpu'] = re_cpu;
+            pmstatus['re_mem'] = re_mem;
+            pmstatus['vm_size'] +=1;
+            self.vm_size+=1;
+            if vm_type not in self.VM.keys():
+                self.VM[vm_type]=0;
+            self.VM[vm_type]+=1;
+            pm = self.PM[pm_id];
+            if vm_type not in pm.keys():
+                pm[vm_type]=0;
+            pm[vm_type]+=1;
+            return (re_cpu,re_mem);
+        return None; # 超分返回
 
 
 
--- src/main/predictor.py
+++ src/main/predictor.py
@@ -2,6 +2,8 @@
 
 import CaseProcess;
 import ParamInfo;
+import packing_model;
+
 
 def predict_vm(ecs_lines, input_lines):
     # Do your work from here#
@@ -20,4 +22,20 @@ def predict_vm(ecs_lines, input_lines):
     print(keys.sort());
     print(keys);
     print(case.get_his_data_by_vmtype('flavor2'));
+    
+    group = packing_model.MachineGroup(case);
+    print(group.put_vm(0, 'flavor13'));
+    print(group.put_vm(0, 'flavor13'));
+    print(group.put_vm(0, 'flavor13'));
+    print(group.put_vm(0, 'flavor13'));
+    print(group.PM_status);
+    group.new_physic_machine();
+    group.new_physic_machine();
+    group.new_physic_machine();
+    group.new_physic_machine();
+    print(group.put_vm(2, 'flavor15'));
+    print(group.put_vm(2, 'flavor15'));
+    print(group.put_vm(2, 'flavor15'));
+    print(group.put_vm(2, 'flavor15'));    
+    print(group.put_vm(2, 'flavor15'));
     return result
